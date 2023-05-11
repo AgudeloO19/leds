19db33a0af9aa492073bae0f994684b1f22f82be
AgudeloO19 <127375696+AgudeloO19@users.noreply.github.com>
2023-05-10 22:35:05 -0500

    Creacion de atributos

--- src/main/java/co/edu/umanizales/leds/model/Led.java
+++ src/main/java/co/edu/umanizales/leds/model/Led.java
@@ -1,4 +1,29 @@
 package co.edu.umanizales.leds.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+
 public class Led {
+
+private int cd;
+
+private boolean status;
+
+private LocalTime dateon;
+
+private LocalTime dateoff;
+
+    public Led(int cd, boolean status){
+        this.cd = cd;
+        this.status = status;
+    }
+
+}
+
+
 }
--- src/main/java/co/edu/umanizales/leds/model/ListDE.java
+++ src/main/java/co/edu/umanizales/leds/model/ListDE.java
@@ -1,4 +1,19 @@
 package co.edu.umanizales.leds.model;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 
 public class ListDE {
+
+    private NodeDE head;
+    private NodeDE tail;
+    private int size;
+    private List<Led> leds = new ArrayList<>();
+
+
+
+
+
 }
