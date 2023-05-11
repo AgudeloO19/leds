693f63c09632722a94203fb40547ebb4061bc20a
AgudeloO19 <127375696+AgudeloO19@users.noreply.github.com>
2023-05-11 11:50:32 -0500

    creacion de metodos principales

--- src/main/java/co/edu/umanizales/leds/model/Led.java
+++ src/main/java/co/edu/umanizales/leds/model/Led.java
@@ -26,4 +26,4 @@ private LocalTime dateoff;
 }
 
 
-}
+
--- src/main/java/co/edu/umanizales/leds/model/ListDE.java
+++ src/main/java/co/edu/umanizales/leds/model/ListDE.java
@@ -12,6 +12,55 @@ public class ListDE {
     private int size;
     private List<Led> leds = new ArrayList<>();
 
+    public void addLed(Led led) {
+
+        int size = 0;
+
+        if (head == null) {
+            head = new NodeDE(led);
+        } else {
+            NodeDE newNode = new NodeDE(led);
+            NodeDE current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+            newNode.setPrevious(current);
+        }
+        size++;
+    }
+    public List <Led> print(){
+        leds.clear();
+        if (head != null){
+            NodeDE temp = head;
+            while (temp != null){
+                leds.add(temp.getData());
+                temp = temp.getNext();
+            }
+        }
+        return leds;
+    }
+
+    public void restart (){
+        NodeDE temp = head;
+        while (temp != null){
+            temp.getData().setStatus(false);
+            temp.getData().setDateoff(null);
+            temp.getData().setDateon(null);
+
+            temp = temp.getNext();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
